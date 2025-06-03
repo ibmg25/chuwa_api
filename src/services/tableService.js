8c858c57b17703ac26dd89fd28565247bc7a2847
ibmg25 <ignacio.martinez@ucb.edu.bo>
2025-06-03 13:03:59 -0400

    getTablesByRestaurant y getTableByLinkCode implementados

--- src/services/tableService.js
+++ src/services/tableService.js
@@ -24,3 +24,13 @@ exports.updateTable = async (id, data) => {
 exports.deleteTable = async (id) => {
   await collection.doc(id).delete();
 };
+
+exports.getTablesByRestaurant = async (restaurantID) => {
+  const snapshot = await collection.where("restaurantID", "==", restaurantID).get();
+  return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+};
+
+exports.getTableByLinkCode = async (code) => {
+  const snapshot = await collection.where("linkCode", "==", code).get();
+  return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+};
